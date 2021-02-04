7d72cc67f14641421649c967caa77373e833bfca
Aaron Janes <Aaron.Janes@gmail.com>
2021-02-04 14:04:03 -0700

    update querries

--- bootcamp_app/students.js
+++ bootcamp_app/students.js
@@ -1,23 +1,24 @@
-const { Pool } = require('pg');
+const { Pool } = require("pg");
 
 const pool = new Pool({
-  user: 'vagrant',
-  password: '123',
-  host: 'localhost',
-  database: 'bootcampx'
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx",
 });
 
-
-
-pool.query(`
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
+const cohort_name = process.argv[2];
+const limit = process.argv[3];
+const values = [`%${cohort_name}%`, limit];
+const queryString = `SELECT students.id, students.name as name, cohorts.name as cohort
 FROM students
-JOIN cohorts ON cohorts.id = cohort_id
-WHERE cohorts.name LIKE '%${process.argv[2]}%'
-LIMIT ${process.argv[3] || 5};
-`)
-.then(res => {
-  res.rows.forEach(user => {
-    console.log(`${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`);
+JOIN cohorts ON cohort_id = cohorts.id
+WHERE cohorts.name Like $1
+LIMIT $2
+`;
+pool
+  .query(queryString, values)
+  .then((res) => {
+    console.log(res);
   })
-}).catch(err => console.error('query error', err.stack));
+  .catch((err) => console.error("error", err.stack));
--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,14 +1,27 @@
-pool.query(`
-SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
-FROM teachers
-JOIN assistance_requests ON teacher_id = teachers.id
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx",
+});
+
+const cohort_name = process.argv[2];
+const values = [`%${cohort_name}%`];
+const queryString = `
+Select distinct teachers.name as teacher, cohorts.name as cohort
+FROM assistance_requests
+JOIN teachers ON  teacher_id = teachers.id
 JOIN students ON student_id = students.id
 JOIN cohorts ON cohort_id = cohorts.id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}'
-ORDER BY teacher;
-`)
-.then(res => {
-  res.rows.forEach(row => {
-    console.log(`${row.cohort}: ${row.teacher}`);
+WHERE cohorts.name LIKE $1
+ORDER BY teachers.name 
+`;
+
+pool
+  .query(queryString, values)
+  .then((res) => {
+    console.log(res);
   })
-});
+  .catch((err) => console.error("error", err.stack));
